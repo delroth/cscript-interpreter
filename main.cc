601962f755829316837783b3de0e62d0677a3afd
Pierre Bourdon <delroth@gmail.com>
2011-03-11 01:57:15 +0100

    Added a simple interpreter driver (which currently only loads the bytecode)

--- main.cc
+++ main.cc
@@ -1,6 +1,26 @@
 #include "cscript.hh"
 
-int main()
+#include <boost/iostreams/device/mapped_file.hpp>
+#include <cstdio>
+#include <string>
+
+int main(int argc, char** argv)
 {
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s <script.so>\n", argv[0]);
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    boost::iostreams::mapped_file_source src(filename);
+
+    cscript::cscript script;
+    if (!script.parse_bytecode(src.data(), src.size()))
+    {
+        fprintf(stderr, "error: %s is not a valid bytecode\n", argv[1]);
+        return 2;
+    }
+
     return 0;
 }
