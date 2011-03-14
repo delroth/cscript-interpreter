cfa4ebc0cd4367e8450e1b8edf7a4673d6243661
Pierre Bourdon <delroth@gmail.com>
2011-03-14 04:30:26 +0100

    Renamed the pop (01 00) instruction to popvar : it is not the reciprocal instruction of push

--- instrs/compute-utils.cc
+++ instrs/compute-utils.cc
@@ -3,11 +3,11 @@
 
 namespace cscript { namespace instruction {
 
-void pop_handler(cscript& interp, uint32_t opcode)
+void popvar_handler(cscript& interp, uint32_t opcode)
 {
     (void)opcode;
     interp.curr_thread().scratch.pop();
 }
-register_instruction pop_instr(0x01000000, 0xFFFF0000, pop_handler);
+register_instruction popvar_instr(0x01000000, 0xFFFF0000, popvar_handler);
 
 }}
