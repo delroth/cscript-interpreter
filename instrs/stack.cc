7677a450aa8d1940f188499267d4151800c4bdcd
Pierre Bourdon <delroth@gmail.com>
2011-03-14 02:05:32 +0100

    Added an handler for the unreserve instruction (opcode 10)

--- instrs/stack.cc
+++ instrs/stack.cc
@@ -12,4 +12,13 @@ void reserve_handler(cscript& interp, uint32_t opcode)
 }
 register_instruction reserve_instr(0x11000000, 0xFF000000, reserve_handler);
 
+void unreserve_handler(cscript& interp, uint32_t opcode)
+{
+    (void)opcode;
+
+    uint32_t bytes_to_unres = interp.read_code_at(interp.curr_thread().pc++);
+    interp.curr_thread().stk.unreserve(bytes_to_unres / 4);
+}
+register_instruction unreserve_instr(0x10000000, 0xFF000000, unreserve_handler);
+
 }}
