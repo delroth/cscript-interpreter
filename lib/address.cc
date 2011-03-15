328cea35ba97398a645b23f2e37d7c2843cc17ae
Pierre Bourdon <delroth@gmail.com>
2011-03-15 04:59:20 +0100

    Removed a bit of code in address.cc which was already in boost MPL/type_traits

--- lib/address.cc
+++ lib/address.cc
@@ -1,25 +1,13 @@
 #include "address.hh"
 
+#include <boost/mpl/identity.hpp>
+#include <boost/type_traits/add_const.hpp>
+
 namespace cscript { namespace address {
 
-/**
- * Metaprogramming to code the get_ptr function without repeating code.
- */
-template <typename T>
-struct Const
-{
-    typedef const T type;
-};
-
-template <typename T>
-struct Normal
-{
-    typedef T type;
-};
-
-template <template <typename> class ConstQual>
-typename ConstQual<char>::type* generic_get_ptr(
-                        typename ConstQual<cscript>::type& script,
+template <template <typename> class trait>
+typename trait<char>::type* generic_get_ptr(
+                        typename trait<cscript>::type& script,
                         uint32_t address)
 {
     if (address & DATA_FLAG)
@@ -32,7 +20,7 @@ typename ConstQual<char>::type* generic_get_ptr(
         uint16_t off = (address & STACK_OFF_MASK) >> STACK_OFF_SHIFT;
         uint16_t tid = (address & STACK_THREAD_MASK) >> STACK_THREAD_SHIFT;
         auto ptr = &script.thread(tid).stk.at(off);
-        return (typename ConstQual<char>::type*)(ptr);
+        return (typename trait<char>::type*)(ptr);
     }
     else
         return 0;
@@ -40,12 +28,12 @@ typename ConstQual<char>::type* generic_get_ptr(
 
 char* get_ptr(cscript& script, uint32_t address)
 {
-    return generic_get_ptr<Normal>(script, address);
+    return generic_get_ptr<boost::mpl::identity>(script, address);
 }
 
 const char* get_ptr(const cscript& script, uint32_t address)
 {
-    return generic_get_ptr<Const>(script, address);
+    return generic_get_ptr<boost::add_const>(script, address);
 }
 
 }}
