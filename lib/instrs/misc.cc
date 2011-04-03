897c287be878bcb6246559ad97647e54addbe56e
Pierre Bourdon <delroth@gmail.com>
2011-04-04 01:51:49 +0200

    Implemented the FORCETYPE instruction (opcode 14)

--- lib/instrs/misc.cc
+++ lib/instrs/misc.cc
@@ -1,5 +1,6 @@
 #include "../cscript.hh"
 #include "../instruction-register.hh"
+#include "../type.hh"
 
 namespace cscript { namespace instruction {
 
@@ -8,6 +9,20 @@ void exit_handler(cscript& interp, uint32_t opcode)
     (void)opcode;
     interp.exit();
 }
+
 register_instruction exit_instr(0x07000000, 0xFF000000, exit_handler);
+
+void forcetype_handler(cscript& interp, uint32_t opcode)
+{
+    uint16_t new_type = type::IMMEDIATE | ((opcode >> 16) & 0xFF);
+    uint16_t elem_size = opcode & 0xFFFF;
+
+    variable& var = interp.curr_thread().scratch.top(0);
+    var.type = new_type;
+    var.pointed_size = elem_size;
+}
+
+register_instruction forcetype_instr(0x14000000, 0xFF000000,
+                                     forcetype_handler);
 
 }}
