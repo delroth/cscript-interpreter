e979643c53842725353c3d8c66cee1fbadc846bf
Pierre Bourdon <delroth@gmail.com>
2011-04-04 14:52:22 +0200

    Dispatch syscalls to the cscript interpreter in the opcode handler

--- lib/instrs/control-flow.cc
+++ lib/instrs/control-flow.cc
@@ -11,12 +11,18 @@ void call_handler(cscript& interp, uint32_t opcode)
 
     uint32_t target = interp.read_code_at(interp.curr_thread().pc++);
     if (target == 0xFFFFFFFF)
-        throw exception("call: syscalls are not yet supported");
+    {
+        // TODO: Handle arguments?
+        uint16_t syscall_id = opcode & 0xFFFF;
+        interp.handle_syscall(syscall_id);
+    }
     else if (target % 4 != 0)
         throw exception("misaligned call target");
-
-    interp.curr_thread().stk.push_frame(interp.curr_thread().pc);
-    interp.curr_thread().pc = target / 4;
+    else
+    {
+        interp.curr_thread().stk.push_frame(interp.curr_thread().pc);
+        interp.curr_thread().pc = target / 4;
+    }
 }
 
 register_instruction call_instr(0x05000000, 0xFF000000, call_handler);
