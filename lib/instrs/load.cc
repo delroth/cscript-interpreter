9f57b384b35d6e2db1ca3aad25439f2675761a93
Pierre Bourdon <delroth@gmail.com>
2011-04-04 02:26:25 +0200

    Implemented the LOAD_DATA_ADDR instruction (opcode 0282 / 0284)

--- lib/instrs/load.cc
+++ lib/instrs/load.cc
@@ -1,19 +1,44 @@
+#include "../address.hh"
 #include "../cscript.hh"
 #include "../instruction-register.hh"
 #include "../type.hh"
 
 namespace cscript { namespace instruction {
 
-void load_schar_handler(cscript& interp, uint32_t opcode)
+namespace {
+
+void load_immediate(cscript& interp, uint32_t value, uint8_t val_type)
 {
     variable& var = interp.curr_thread().scratch.top(0);
-    var.value.u32 = (int8_t)(opcode & 0xFF);
+    var.value.u32 = value;
     var.address = 0;
-    var.type = type::IMMEDIATE | type::SCHAR;
+    var.type = type::IMMEDIATE | val_type;
+    var.pointed_size = 0;
     interp.curr_thread().scratch.push();
+}
+
+}
+
+void load_schar_handler(cscript& interp, uint32_t opcode)
+{
+    load_immediate(interp, (int8_t)(opcode & 0xFF), type::SCHAR);
 }
 
 register_instruction load_schar_instr(0x02030000, 0xFFFF0000,
                                       load_schar_handler);
+
+void load_data_addr_handler(cscript& interp, uint32_t opcode)
+{
+    (void)opcode;
+
+    uint32_t addr = interp.read_code_at(interp.curr_thread().pc++);
+    load_immediate(interp, address::make_data_addr(addr),
+                   type::POINTER | type::UCHAR);
+}
+
+register_instruction load_data_addr_instr(0x02820000, 0xFFFF0000,
+                                          load_data_addr_handler);
+register_instruction load_data_addr_instr2(0x02840000, 0xFFFF0000,
+                                           load_data_addr_handler);
 
 }}
