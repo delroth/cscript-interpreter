843c8a4950c6688e5447c8a4b44a0ad9ed69594b
Pierre Bourdon <delroth@gmail.com>
2011-05-04 16:04:09 +0200

    Stubbed the unknown skit syscall 0x64.

---
+++ lib/skit/syscall-unknown.hh
@@ -0,0 +1,13 @@
+#ifndef __SKIT_SYSCALL_UNKNOWN_HH_
+# define __SKIT_SYSCALL_UNKNOWN_HH_
+
+# include "../cscript.hh"
+
+namespace cscript { namespace skit { namespace syscalls {
+
+const uint16_t SKIT_UNKNOWN_ID = 0x64;
+void skit_unknown(cscript& script);
+
+}}}
+
+#endif
--- lib/skit/syscalls.cc
+++ lib/skit/syscalls.cc
@@ -1,5 +1,6 @@
 #include "syscalls.hh"
 #include "syscall-init.hh"
+#include "syscall-unknown.hh"
 #include "syscall-wait.hh"
 
 #include <map>
@@ -8,7 +9,8 @@ namespace cscript { namespace skit {
 
 std::map<uint16_t, std::function<void(cscript&)>> handlers = {
     { syscalls::SKIT_INIT_ID, syscalls::skit_init },
-    { syscalls::SKIT_WAIT_ID, syscalls::skit_wait }
+    { syscalls::SKIT_WAIT_ID, syscalls::skit_wait },
+    { syscalls::SKIT_UNKNOWN_ID, syscalls::skit_unknown },
 };
 
 bool execute_syscall(cscript& script, uint16_t syscall)
