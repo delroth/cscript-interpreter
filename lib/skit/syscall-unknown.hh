738726ebf6b43b8131a8970ad83c3e3dfa5da6af
Pierre Bourdon <delroth@gmail.com>
2011-05-07 02:37:21 +0200

    Stubbed yet another unknown skit syscall (id 65) for FC03_007.

--- lib/skit/syscall-unknown.hh
+++ lib/skit/syscall-unknown.hh
@@ -9,17 +9,18 @@ namespace cscript { namespace skit { namespace syscalls {
     const uint16_t SKIT_UNKNOWN##n##_ID = id; \
     void skit_unknown##n(cscript& script, const std::vector<uint32_t>& args);
 
-UNKNOWN_DECL(1, 0x6B)
-UNKNOWN_DECL(2, 0x6C)
-UNKNOWN_DECL(3, 0x6D)
-UNKNOWN_DECL(4, 0x6E)
-UNKNOWN_DECL(5, 0x6F)
-UNKNOWN_DECL(6, 0x70)
-UNKNOWN_DECL(7, 0x72)
-UNKNOWN_DECL(8, 0x73)
-UNKNOWN_DECL(9, 0x77)
-UNKNOWN_DECL(10, 0x7e)
-UNKNOWN_DECL(11, 0x7f)
+UNKNOWN_DECL(1, 0x65)
+UNKNOWN_DECL(2, 0x6B)
+UNKNOWN_DECL(3, 0x6C)
+UNKNOWN_DECL(4, 0x6D)
+UNKNOWN_DECL(5, 0x6E)
+UNKNOWN_DECL(6, 0x6F)
+UNKNOWN_DECL(7, 0x70)
+UNKNOWN_DECL(8, 0x72)
+UNKNOWN_DECL(9, 0x73)
+UNKNOWN_DECL(10, 0x77)
+UNKNOWN_DECL(11, 0x7e)
+UNKNOWN_DECL(12, 0x7f)
 
 const uint16_t SKIT_INITSOMETHING_ID = 0x64;
 void skit_initsomething(cscript& script, const std::vector<uint32_t>& args);
