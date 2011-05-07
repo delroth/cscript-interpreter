9e56ecdd801995021308bef3c80f78ebe6fb2fb0
Pierre Bourdon <delroth@gmail.com>
2011-05-07 02:46:23 +0200

    Stubbed skit wait syscall 6A for FC03_033.

--- lib/skit/syscall-wait.hh
+++ lib/skit/syscall-wait.hh
@@ -9,6 +9,7 @@ const uint16_t SKIT_WAIT_ID = 0x7A;
 const uint16_t SKIT_WAIT2_ID = 0x7B;
 const uint16_t SKIT_WAIT3_ID = 0x69;
 const uint16_t SKIT_WAIT4_ID = 0x7C;
+const uint16_t SKIT_WAIT5_ID = 0x6A;
 void skit_wait(cscript& script, const std::vector<uint32_t>& args);
 
 }}}
